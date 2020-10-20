4b4c948a90933351ece3a45d4b977e91d868a974
MinGwang Song <43804413+alsrhkd77@users.noreply.github.com>
2020-10-20 20:07:39 +0900

    Update WebController.java

--- src/main/java/com/example/tripstory/WebController.java
+++ src/main/java/com/example/tripstory/WebController.java
@@ -22,9 +22,9 @@ public class WebController {
     }
 
     @GetMapping("/getTuples")
-    public List<String> getTuples() {
+    public String getTuples() {
         return this.jdbcTemplate.queryForList("SELECT * FROM test").stream()
                 .map((m) -> m.values().toString())
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()).toString();
     }
 }
