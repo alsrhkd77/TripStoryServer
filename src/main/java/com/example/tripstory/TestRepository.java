39e5566c2f301072fc8ff0c782d71feea9f7bbfc
MinGwang Song <43804413+alsrhkd77@users.noreply.github.com>
2020-10-21 17:21:42 +0900

    Update TestRepository.java

--- src/main/java/com/example/tripstory/TestRepository.java
+++ src/main/java/com/example/tripstory/TestRepository.java
@@ -7,6 +7,4 @@ import java.util.List;
 
 @Repository
 public interface TestRepository extends JpaRepository<TestEntitiy, Integer> {
-    public List<TestEntitiy> findById(int id);
-    public List<TestEntitiy> findByPw(int pw);
 }
