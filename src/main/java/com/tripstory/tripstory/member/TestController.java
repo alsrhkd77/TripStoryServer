256c34d13f16308b4d14e8216a34944bf20e5f1a
CodeMechanic <jeonseun0000@gmail.com>
2020-10-26 00:34:12 +0900

    gcp jpa 작동 확인7

--- src/main/java/com/tripstory/tripstory/member/TestController.java
+++ src/main/java/com/tripstory/tripstory/member/TestController.java
@@ -2,20 +2,25 @@ package com.tripstory.tripstory.member;
 
 import com.tripstory.tripstory.member.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class TestController {
 
     @Autowired
     private TestService testService;
 
-    @GetMapping("/{id}/{name}/{email}")
-    public String createMember(@PathVariable("id") String id,
-                               @PathVariable("name") String name,
-                               @PathVariable("email") String email) {
+    @GetMapping("/form")
+    public String memberCreateForm() {
+        return "member/signUpForm";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String createMember(@RequestParam String id,
+                               @RequestParam String name,
+                               @RequestParam String email) {
         Member member = Member.builder()
                 .id(id)
                 .name(name)
