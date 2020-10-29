2916e89c10ece0a6e71c324543272557b79b02f7
jeonseun <wjstpdns2015@naver.com>
2020-10-29 18:59:06 +0900

    게시물 생성 구현
    - GCP 환경 작동 테스트중 3

--- src/main/java/com/tripstory/tripstory/util/GCPFileStorage.java
+++ src/main/java/com/tripstory/tripstory/util/GCPFileStorage.java
@@ -3,31 +3,31 @@ package com.tripstory.tripstory.util;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import java.util.UUID;
 
 @Component
 @Profile("prod")
 public class GCPFileStorage implements FileStorage{
+
+    @Value("${resources.post-image.path}")
+    private String storagePath;
+
     @Override
     public String saveFile(byte[] bytes, String fileName, String contentType) {
-
+        String uuid = UUID.randomUUID().toString();
+        String name = uuid + fileName;
         try {
             Storage storage = StorageOptions.getDefaultInstance().getService();
             BlobInfo blobInfo = storage.create(
-                    BlobInfo.newBuilder("tripstory_image", "post_image/" + fileName).setContentType(contentType).build(),
+                    BlobInfo.newBuilder("tripstory_image", "post_image/" + name).setContentType(contentType).build(),
+                    bytes,
                     Storage.BlobTargetOption.predefinedAcl(Storage.PredefinedAcl.PUBLIC_READ) // Set file permission
             );
-            System.out.println("----------------------------");
-            System.out.println(blobInfo.getMediaLink());
-            System.out.println(blobInfo.getSelfLink());
-            System.out.println(blobInfo.getBucket());
-            System.out.println(blobInfo.getGeneratedId());
-            System.out.println(blobInfo.getName());
-            System.out.println("----------------------------");
-            return blobInfo.getSelfLink(); // Return file url
+            return storagePath + blobInfo.getName(); // Return file url
         }catch(IllegalStateException e){
             throw new RuntimeException(e);
         }
