443d100452a593c8b857b244ef3383635bdf031a
jeonseun <wjstpdns2015@naver.com>
2020-11-20 19:40:08 +0900

    파일 업로드 모듈 리팩토링

--- src/main/java/com/tripstory/tripstory/util/GCPFileStorage.java
+++ src/main/java/com/tripstory/tripstory/util/GCPFileStorage.java
@@ -38,19 +38,8 @@ public class GCPFileStorage implements FileStorage {
                     bytes,
                     Storage.BlobTargetOption.predefinedAcl(Storage.PredefinedAcl.PUBLIC_READ) // Set file permission
             );
-            System.out.println("---------------------------------------");
-            System.out.println("---------------------------------------");
-            System.out.println("---------------------------------------");
-            System.out.println("---------------------------------------");
-            System.out.println("---------------------------------------");
-            System.out.println("---------------------------------------");
-            System.out.println("---------------------------------------");
-            System.out.println(blobInfo.getName());
-            System.out.println(blobInfo.getName().replace(location, ""));
-            System.out.println(blobInfo.getBlobId().toString());
-            System.out.println(blobInfo.getBlobId().getName());
-            System.out.println(blobInfo.getGeneratedId());
-            return blobInfo.getName().replace(location, ""); // return object name
+
+            return blobInfo.getName().replace(folderName, ""); // return object name
 
         } catch (IllegalStateException e) {
             throw new RuntimeException(e);
